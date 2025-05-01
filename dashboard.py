9ae9f243980b672093cec89caa2941898a41ab78
Priyanshu <priyanshuatmit@gmail.com>
2025-05-01 15:39:23 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from pathlib import Path
 
 st.set_page_config(page_title="Football League Analysis",layout="wide", page_icon="⚽")
 
